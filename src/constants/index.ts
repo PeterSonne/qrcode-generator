219dbb76ab18c50f8cf67298f05665e4fd90d9de
Peter Sonne <papa@deep2chill.de>
2024-01-14 23:33:43 +0100

    chore: provide encoding mode specific constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,20 @@
+import { MODE } from '../types';
+
 export const CHARACTER_INDICATORS_COUNT_NUMBERIC = [10, 12, 14];
 export const CHARACTER_INDICATORS_COUNT_ALPHA_NUMBERIC = [9, 11, 13];
 export const CHARACTER_INDICATORS_COUNT_BYTE = [8, 16, 16];
+
+export const MODE_PROPERTIES = {
+  [MODE.NUMERIC]: {
+    code: 1,
+    lengthCharacterIndicator: CHARACTER_INDICATORS_COUNT_NUMBERIC,
+  },
+  [MODE.ALPHA_NUMERIC]: {
+    code: 2,
+    lengthCharacterIndicator: CHARACTER_INDICATORS_COUNT_NUMBERIC,
+  },
+  [MODE.BYTE]: {
+    code: 4,
+    lengthCharacterIndicator: CHARACTER_INDICATORS_COUNT_NUMBERIC,
+  },
+};
